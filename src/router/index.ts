4213842f2054ac0f2388ae00a539b0319e36d494
yal <643589668@qq.com>
2022-10-26 15:07:18 +0800

    重定向

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import 'nprogress/css/nprogress.css' //进度条样式
 import { createRouter, createWebHashHistory } from 'vue-router'
 
+import { createRouterGuards } from './router-guards';
+import { whiteNameList } from './constant';
+
 import type { RouteRecordRaw } from 'vue-router'
 import type { App } from 'vue'
 
@@ -25,9 +28,20 @@ export const router = createRouter({
   history:createWebHashHistory(),
   routes
 })
+
+//rest router
+export function resetRouter() {
+  router.getRoutes().forEach((route) => {
+    const { name } = route;
+    if (name && !whiteNameList.some((n) => n === name)) {
+      router.hasRoute(name) && router.removeRoute(name);
+    }
+  });
+}
+
 export async function setupRouter(app: App) {
-  // 创建路由守卫
-  // createRouterGuards(router, whiteNameList);
+  //创建路由守卫
+  createRouterGuards(router, whiteNameList);
 
   app.use(router);
 
